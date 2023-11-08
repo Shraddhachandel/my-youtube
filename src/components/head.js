b2e7f5eaf36ea754eec7813e5967bbdd003a7acf
unknown <shraddhachandel1997@gmail.com>
2023-11-08 22:52:09 +0530

    Addressing synax errors

--- src/components/head.js
+++ src/components/head.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
 import { YOUTUBE_SEARCH_API } from "../utils/contants";
@@ -7,12 +7,12 @@ import { cacheResults } from "../utils/searchSlice";
 const head = () => {
    const [searchQuery, setSearchQuery] = useState("");
    const [suggestions, setSuggestions] = useState([]);
-  const [showSuggestions, setShowSuggestions] = useState(false);
+   const [showSuggestions, setShowSuggestions] = useState(false);
 
    const searchCache = useSelector((store) => store.search);
   const dispatch = useDispatch();
    useEffect(() => {
-    const timer = setTimeout(() => 
+    const timer = setTimeout(() =>
      {
        if (searchCache[searchQuery]) {
          setSuggestions(searchCache[searchQuery]);
@@ -30,27 +30,19 @@ const head = () => {
      const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
      const json = await data.json();
      setSuggestions(json[1]);
-  
-     const dispatch = useDispatch();
+ 
+     dispatch(cacheSuggestions({ [searchQuery]: json[1] }));
      
-   }
-
-  //   // update cache
-     dispatch(
-     cacheResults({
-      [searchQuery]: json[1],
-       }  )
-      );
    };
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
-  
+ 
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-1">
-        <img
+      <img
           onClick={() => toggleMenuHandler()}
           className="h-8 cursor-pointer"
           alt="menu"
@@ -65,13 +57,6 @@ const head = () => {
         </a>
       </div>
       <div className="col-span-10 px-10">
-        <input
-          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
-          type="text"
-        />
-        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
-          🔍
-        </button>
         <div>
           <input
             className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
@@ -105,6 +90,7 @@ const head = () => {
         />
       </div>
     </div>
-  );
-
+ 
+    );
+  };
 export default head;
